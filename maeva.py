0b471cac772126eee82f31a0e0e4f7c4510c8777
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-07-12 12:46:49 +0300

    hotels et maeva

---
+++ maeva.py
@@ -0,0 +1,50 @@
+from scraping import Scraping
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from abc import abstractmethod
+import sys
+import time
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, parse_qs
+from langdetect import detect
+
+
+class Maeva(Scraping):
+    def __init__(self, in_background: bool = False, url: str = "") -> None:
+        super().__init__(in_background, url=url)
+        self.url = url
+        self.reviews = []
+
+    def load_reviews(self) -> None:
+        results = int(''.join(filter(str.isdigit, self.driver.find_element(By.ID, 'avis-comp-content').find_element(By.CLASS_NAME, 'ml-1').text)))
+        for i in range(results//3):
+            self.driver.find_element(By.ID, 'avis-cards-content-container').click()
+            for k in range(3):
+                self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+            time.sleep(2)
+
+    def extract(self) -> None:
+
+        self.load_reviews()
+
+        try:
+            soup  = BeautifulSoup(self.driver.page_source, 'lxml')
+            reviews = soup.find('div', {'id':'avis-cards-content-container'}).find_all('div', {'typeof':'comment'})
+            for review in reviews:
+                data = {}
+                data['author'] = review.find('div', class_='date-publication').find('strong').text.strip()
+                data['date_post'] = review.find('span', {'property':'dateCreated'})['content']
+                data['comment'] = review.find('p', class_='avis-comment').text.strip() if review.find('p', class_='avis-comment') else ''
+                data['rating'] = review.find('span', class_='score-text').text if review.find('span', class_='score-text') else 0
+                self.reviews.append(data)
+        except Exception as e:
+            print('extraction file')
+            print(e)
+
+
+trp = Maeva(url="https://www.maeva.com/fr-fr/residence-pierre---vacances-douchka_21505.html")
+trp.execute()
