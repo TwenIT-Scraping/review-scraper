bacc8354a739127c53791ec34e5112618c6c9540
Thierry Keller <83826994+thierryKeller-1@users.noreply.github.com>
2023-07-12 18:08:44 +0300

    'hotels'

--- hotels.py
+++ hotels.py
@@ -32,9 +32,17 @@ class Hotels(Scraping):
         button_review = self.driver.find_element(By.CSS_SELECTOR, '#Reviews > div > div > div:nth-child(2) > div > button')
         try:
             button_review.click()
+            try:
+                button_review.click()
+            except:
+                pass
             time.sleep(2)
             button_view_more = self.driver.find_element(By.CSS_SELECTOR, '#app-layer-reviews-property-reviews-1 > section > div.uitk-sheet-content.uitk-sheet-content-padded.uitk-sheet-content-extra-large > div > div.uitk-layout-grid.uitk-layout-grid-align-content-start.uitk-layout-grid-has-auto-columns.uitk-layout-grid-has-columns.uitk-layout-grid-has-space.uitk-layout-grid-display-grid.uitk-layout-grid-item.uitk-layout-grid-item-has-column-start.uitk-layout-grid-item-has-column-start-by-medium.uitk-layout-grid-item-has-column-start-by-large.uitk-layout-grid-item-has-column-start-by-extra_large > div.uitk-layout-grid-item > section > div.uitk-spacing.uitk-type-center.uitk-spacing-margin-block-three > button')
             while button_view_more.is_displayed():
+                try:
+                    self.driver.find_element(By.CSS_SELECTOR, '#app-layer-recommendations-overlay > section > div.uitk-layout-flex.uitk-layout-flex-align-items-center.uitk-toolbar > button').click()
+                except:
+                    pass
                 button_view_more.click()
                 WebDriverWait(self.driver, 5)
                 time.sleep(1)
@@ -42,19 +50,39 @@ class Hotels(Scraping):
             pass
 
     def extract(self) -> None:
-
+        def fomat_date(date:str) -> str:
+            date_format = {
+                'janv.':'01',
+                'févr.':'02',
+                'mars':'03',
+                'avr.':'04',
+                'mai': '05',
+                'juin':'06',
+                'juil.':'07',
+                'août':'08',
+                'sept.':'09',
+                'oct.':'10',
+                'nov.':'11',
+                'déc.':'12'
+            }
+            date = date.split(' ')
+            return f'{date[0]}/{date_format[date[1]]}/{date[2]}'
         self.load_reviews()
 
         soup = BeautifulSoup(self.driver.page_source, 'lxml')
         reviews = soup.find('div', {'data-stid':'property-reviews-list'}).find_all('article', {'itemprop':'review'})
         for review in reviews:
             data = {}
-            data['date_post'] = review.find('span', {'itemprop':'datePublished'}).text.strip()
+            data['date_review'] = fomat_date(review.find('span', {'itemprop':'datePublished'}).text.strip())
             data['author'] = review.find('img').parent.text.split(',')[0]
-            data['rating'] = review.find('span', {'itemprop':'ratingValue'}).text.split(' ')[0]
+            data['rating'] = review.find('span', {'itemprop':'ratingValue'}).text.split(' ')[0] if review.find('span', {'itemprop':'ratingValue'}) else '0'
             data['comment'] = review.find('span', {'itemprop':'description'}).text if review.find('span', {'itemprop':'description'}) else ''
+            data['language'] = detect(data['comment']) if data['comment'] else ''
+            data['establishement'] = '/api/establishments/3'
+            print(data)
             self.reviews.append(data)
-
+        print(len(self.reviews))
+        print(self.reviews)
 
 trp = Hotels(url="https://fr.hotels.com/ho1100722624/okko-hotels-paris-gare-de-l-est-paris-france")
 trp.execute()
--- scraping.py
+++ scraping.py
@@ -36,11 +36,11 @@ class Scraping(object):
         in_background and self.firefox_options.add_argument('--headless')
         self.firefox_options.add_argument('--incognito')
 
-        self.driver = webdriver.Firefox(service=FirefoxService(
-                    GeckoDriverManager().install()), options=self.firefox_options)
+        # self.driver = webdriver.Firefox(service=FirefoxService(
+        #             GeckoDriverManager().install()), options=self.firefox_options)
 
-        # self.driver = webdriver.Chrome(service=ChromeService(
-        #     ChromeDriverManager().install()), options=self.chrome_options)
+        self.driver = webdriver.Chrome(service=ChromeService(
+            ChromeDriverManager().install()), options=self.chrome_options)
 
         self.driver.maximize_window()
         self.current_driver = 'firefox'
