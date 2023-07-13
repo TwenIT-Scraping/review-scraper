745151373fdad4935070e66fb3b678decd110a67
Johary <joharyandrianjafimanohisolo@gmail.com>
2023-07-13 11:06:24 +0300

    calcul score

--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
-# from transformers import AutoTokenizer, AutoModelForSequenceClassifier, pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassifier, pipeline
+from api import ERApi
+from random import randint, random
 
 
 months_fr = {
@@ -34,11 +36,38 @@ months_en = {
 def month_number(name, lang):
     return globals()[f"months_{lang}"][name]
 
-# def get_score(text):
-#     model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
-#     model = AutoModelForSequenceClassification.from_pretrained(model_name)
-#     tokenizer = AutoTokenizer.from_pretrained(model_name)
-#     classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-#     print(classifier(text))
+def get_score(text):
+    model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+    print(classifier(text))
 
 # get_score("Leur service n'est pas au top")
+
+def random_score():
+
+    for review_id in range(2168, 4496):
+        print("set value for ", review_id)
+        patch_instance = ERApi(method='put', entity='reviews', id=review_id)
+        score = float("%.2f" % random())
+        feeling = ["negative", "neutre", "positive"][randint(0,2)]
+
+        if feeling == "negative":
+            confidence = -1 * score
+        elif feeling == "neutre":
+            confidence = 0
+        else:
+            confidence = score
+        
+        body = {"feeling": feeling, "confidence": confidence, "score": score}
+
+        patch_instance.set_body(body)
+
+        try:
+            res = patch_instance.execute()
+        except Exception as e:
+            print(e)
+
+
+# random_score()
